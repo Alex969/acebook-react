16286570c11506726dad887ea52f81732b3199f4
ConorButler <cwdbutler@gmail.com>
2021-11-19 10:08:34 +0000

    Post feed shows likes and comments

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,12 +4,15 @@ import React from 'react'
 //import ListItemText from '@mui/material/ListItemText';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import timeSince from '../timeSince'
 
 const Post = (response) => {
+
+
   return (
     <div className="container">
       <Typography variant="h5" gutterBottom component="div">
-        {response.post.user.first_name} {response.post.user.last_name}
+        {response.post.user.first_name} {response.post.user.last_name} {response.post.created_at}
       </Typography>
       <Box component="span"
         sx={{
@@ -22,7 +25,35 @@ const Post = (response) => {
         }}>
         {response.post.content}
       </Box>
-      </div>
+      {(() => {
+        if (response.post.like_count == 1) {
+          return (
+            <div>{response.post.like_count} like</div>
+          )
+        } else if (response.post.like_count > 1) {
+          return (
+            <div>{response.post.like_count} likes</div>
+          )
+        }
+      })()}
+      {(() => {
+        if (response.post.comment_count > 0) {
+          return (
+            <>
+              <div>{response.post.comment_count} comments</div>
+              <div>
+                {response.post.comments[0].user.first_name} {" "}
+                {response.post.comments[0].user.last_name}
+              </div>
+              <div>{response.post.comments[0].content}</div>
+              <div>{response.post.comments[0].like_count} likes</div>
+            </>
+          )
+        } else {
+          return 
+        }
+      })()}
+    </div>
   )
 }
 
---
+++ src/timeSince.js
@@ -0,0 +1,33 @@
+// replicates twitter date formatting
+const timeSince = (dateObject) => {  
+  const monthFormat = { month: 'short' };
+
+  let yearStartDate = new Date(new Date().getFullYear(), 0, 1);
+  let yearStart = yearStartDate.getTime() / 1000
+  let time = Math.floor(dateObject.getTime() / 1000)
+  if (time < yearStart) {
+    return `${dateObject.getDate()} ${dateObject.toLocaleDateString("en-US", monthFormat)} ${dateObject.getFullYear()}`;
+    // e.g. 10 Sep 2018
+  }
+
+  let now = (new Date).getTime() / 1000
+  let oneDayAgo = now - 86400
+  if (time < oneDayAgo) {
+    return `${dateObject.getDate()} ${dateObject.toLocaleDateString("en-US", monthFormat)}`;
+    // e.g. 18 Jul
+  }
+  
+  let secondsAgo = Math.floor(now) - time
+  if (secondsAgo > 3600) {
+    // e.g. 4h
+    return `${Math.floor(secondsAgo/3600)}h`
+  } else if (secondsAgo > 60) {
+    // e.g. 32m
+    return `${Math.floor(secondsAgo/60)}m`
+  } else {
+    // e.g. 12s
+    return `${secondsAgo}s`
+  };
+};
+
+export default timeSince
