08ea51b103aadcceaf246176bdc9fcf0087864fb
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-11-19 14:58:49 +0100

    fixed css not showing up issues

--- src/components/Feed/CreatePost.js
+++ src/components/Feed/CreatePost.js
@@ -5,44 +5,46 @@ import "./CreatePost.css"
 const CreatePost = ({ onCreate }) => {
   const [content, setText] = useState('')
   return (
-    <div className="container">
-      <FormControl fullWidth={true} >
-        <TextField id="post" 
-                    type="text"
-                    placeholder="What's on your mind?"
-                    value={content} onChange={(e) => setText(e.target.value)}
-                    multiline={true}
-                    sx={{
-                      bgcolor: 'background.paper'
-                    }} />
-        <Button type="submit" sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          backgroundColor: '#E2BCFB',
-          border: 0,
-          m: 1,
-          justifyContent: 'center',
-          //mt: 10,
-          //mb: 8,
-          mx: 'auto',
-          borderRadius: 4,
-          color: 'white',
-          fontWeight: 700,
-          width: 500
-        }}
-            onClick={() => {
-              if (!content) {
-                alert('Please add some content') //raises error if you try and submit without entering text
-                return
+    <div className="onyourmind">
+      <div className="container">
+        <FormControl fullWidth={true} >
+          <TextField id="post" 
+                      type="text"
+                      placeholder="What's on your mind?"
+                      value={content} onChange={(e) => setText(e.target.value)}
+                      multiline={true}
+                      sx={{
+                        bgcolor: 'background.paper'
+                      }} />
+          <Button type="submit" sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            backgroundColor: '#E2BCFB',
+            border: 0,
+            m: 1,
+            justifyContent: 'center',
+            //mt: 10,
+            //mb: 8,
+            mx: 'auto',
+            borderRadius: 4,
+            color: 'white',
+            fontWeight: 700,
+            width: 500
+          }}
+              onClick={() => {
+                if (!content) {
+                  alert('Please add some content') //raises error if you try and submit without entering text
+                  return
+                }
+                onCreate({ content })
+                setText("") //resets text
+                }
               }
-              onCreate({ content })
-              setText("") //resets text
-              }
-            }
-          >
-          Post
-        </Button>
-      </FormControl>
+            >
+            Post
+          </Button>
+        </FormControl>
+      </div>
     </div>
     
   )
